b16c6a64b5f460307d7aa59631a26ab19ffdea97
rahul.prasad <rahul.prasad@geekywolf.com>
2025-11-12 19:16:22 +0530

    started using form control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./shared/navbar/navbar.component";
 import { FooterComponent } from "./shared/footer/footer.component";
+import { ReactiveFormsModule} from "@angular/forms"
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, FooterComponent, RouterOutlet],
+  imports: [NavbarComponent, FooterComponent, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ɵInternalFormsSharedModule } from "@angular/forms"
+import { ReactiveFormsModule } from '@angular/forms';
+import { Validators } from "@angular/forms"
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ɵInternalFormsSharedModule, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  loginForm = new FormGroup({
+    email : new FormControl("",[Validators.required, Validators.email]),
+    password : new FormControl("",[Validators.required, Validators.minLength(8)])
+  })
 
+  onSubmit(){
+    console.log(this.loginForm.value)
+  }
 }
